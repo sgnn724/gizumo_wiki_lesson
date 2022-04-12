f8e08fdb211589e5dd7d2ca9550f2006051788fe
aldebarandododo <80578563+aldebarandododo@users.noreply.github.com>
2022-04-12 16:54:59 +0900

    feat: カテゴリー更新画面作成・更新処理追加

--- src/js/_store/modules/categories.js
+++ src/js/_store/modules/categories.js
@@ -66,10 +66,16 @@ export default {
     doneGetAllCategories(state, { categories }) {
       state.categoryList = [...categories].reverse();
     },
+    doneGetCategory(state, payload) {
+      state.targetCategory = Object.assign({}, state.targetCategory, payload.category);
+    },
     updateValue(state, payload) {
       state.targetCategory = Object.assign({}, { ...state.targetCategory }, {
         name: payload.name,
       });
+    },
+    updateCategory(state, { category }) {
+      state.targetCategory = Object.assign({}, state.targetCategory, { ...category });
     },
     displayDoneMessage(state, message = '成功しました') {
       state.doneMessage = message;
@@ -107,10 +113,49 @@ export default {
         commit('failRequest', { message: err.message });
       });
     },
+    getCategoryDetail({ commit, rootGetters }, { id }) {
+      axios(rootGetters['auth/token'])({
+        method: 'GET',
+        url: `/category/${id}`,
+      }).then((res) => {
+        const payload = {
+          category: {
+            id: res.data.category.id,
+            name: res.data.category.name,
+          },
+        };
+        commit('doneGetCategory', payload);
+      }).catch((err) => {
+        commit('failRequest', { message: err.message });
+      });
+    },
     updateValue({ commit }, name) {
       commit({
         type: 'updateValue',
         name,
+      });
+    },
+    updateCategory({ commit, rootGetters, state }) {
+      commit('toggleLoading');
+      const data = new URLSearchParams();
+      data.append('name', state.targetCategory.name);
+      axios(rootGetters['auth/token'])({
+        method: 'PUT',
+        url: `/category/${state.targetCategory.id}`,
+        data,
+      }).then((res) => {
+        const payload = {
+          category: {
+            id: res.data.category.id,
+            name: res.data.category.name,
+          },
+        };
+        commit('updateCategory', payload);
+        commit('toggleLoading');
+        commit('displayDoneMessage', 'カテゴリーを更新しました');
+      }).catch((err) => {
+        commit('toggleLoading');
+        commit('failRequest', { message: err.message });
       });
     },
     postCategory({ commit, rootGetters }) {
---
+++ src/js/pages/Categories/Detail.vue
@@ -0,0 +1,63 @@
+<template lang="html">
+  <app-category-detail
+    :category="category"
+    :error-message="errorMessage"
+    :done-message="doneMessage"
+    :loading="loading"
+    :access="access"
+    @clearMessage="clearMessage"
+    @updateValue="updateValue"
+    @handleSubmit="handleSubmit"
+  />
+</template>
+
+<script>
+import { CategoryDetail } from '@Components/molecules';
+import Mixins from '@Helpers/mixins';
+
+export default {
+  components: {
+    appCategoryDetail: CategoryDetail,
+  },
+  mixins: [Mixins],
+  computed: {
+    category() {
+      return this.$store.state.categories.targetCategory;
+    },
+    errorMessage() {
+      return this.$store.state.categories.errorMessage;
+    },
+    doneMessage() {
+      return this.$store.state.categories.doneMessage;
+    },
+    loading() {
+      return this.$store.state.categories.loading;
+    },
+    access() {
+      return this.$store.getters['auth/access'];
+    },
+  },
+  created() {
+    const { id } = this.$route.params;
+    this.$store.dispatch('categories/getCategoryDetail', { id });
+    this.$store.dispatch('categories/clearMessage');
+  },
+  destroyed() {
+    this.$store.dispatch('categories/clearMessage');
+  },
+  methods: {
+    clearMessage() {
+      this.$store.dispatch('categories/clearMessage');
+    },
+    updateValue(target) {
+      if (!this.loading) this.$store.dispatch('categories/updateValue', target.value);
+    },
+    handleSubmit() {
+      this.$store.dispatch('categories/updateCategory', {
+        id: this.category.id,
+        name: this.category.name,
+      });
+    },
+  },
+};
+</script>
