3c61233e6cc495a72943f210397de738e6278c52
山本建治 <kenjibasketball0520@gmail.com>
2022-05-12 11:32:32 +0900

    fix:意図しない改行と不要なconsole.logの削除

--- src/js/components/molecules/CategoryList/index.vue
+++ src/js/components/molecules/CategoryList/index.vue
@@ -15,7 +15,6 @@
       </thead>
       <transition-group name="fade" tag="tbody" class="category-list__table__body">
         <tr v-for="category in categories" :key="category.id">
-          <!-- console.log(category.id); -->
           <td>
             <app-text tag="span">
               {{ category.name }}
